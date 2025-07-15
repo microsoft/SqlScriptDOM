ec8be62ef936032fc9605d5a6d6d0b9b571628d1
C Shanmukha Reddy <chagamreddys@microsoft.com>
2025-07-15 07:21:58 +0000

    Merged PR 1735207: Adding regex functions tests
    # Pull Request Template for ScriptDom
    ## Description
    This PR adds tests for regex intrinsic functions.
    Functional Spec - https://microsoft.sharepoint.com/:w:/t/Ad-Astra/EZAk15L-vkJHqsaCybly0QIB0eBUdv02BBaHUAl48aY_Pw?e=NPRj0w
    
    ## Code Change
    - [x] The [Common checklist](https://msdata.visualstudio.com/SQLToolsAndLibraries/_git/Common?path=/Templates/PR%20Checklist%20for%20SQLToolsAndLibraries.md&version=GBmain&_a=preview) has been reviewed and followed
    - [x] Code changes are accompanied by appropriate unit tests
    - [ ] Identified and included SMEs needed to review code changes
    - [x] Follow the [steps](https://msdata.visualstudio.com/SQLToolsAndLibraries/_wiki/wikis/SQLToolsAndLibraries.wiki/33838/Adding-or-Extending-TSql-support-in-Sql-Dom?anchor=make-the-changes-in) here to make changes in the code
    
    ## Testing
    - [x] Follow the [steps](https://msdata.visualstudio.com/SQLToolsAndLibraries/_wiki/wikis/SQLToolsAndLibraries.wiki/33838/Adding-or-Extending-TSql-support-in-Sql-Dom?anchor=to-extend-the-tests-do-the-following%3A) here to add new tests for your feature
    
    ## Documentation
    - [ ] Update relevant documentation in the [wiki](https://dev.azure.com/msdata/SQLToolsAndLibraries/_wiki/wikis/SQLToolsAndLibraries.wiki) and the README.md file
    
    ## Additional Information
    None
    
    Adding regex functions tests
    
    Related work items: #4005966

---
+++ Test/SqlDom/Baselines170/RegexpTests170.sql
@@ -0,0 +1,22 @@
+﻿SELECT REGEXP_COUNT('hello', 'he(l+)o');
+SELECT REGEXP_COUNT('hello', 'he(l+)o', 1);
+SELECT REGEXP_COUNT('hello', 'he(l+)o', 1, 'i');
+
+SELECT REGEXP_INSTR('hello', 'he(l+)o');
+SELECT REGEXP_INSTR('hello', 'he(l+)o', 1);
+SELECT REGEXP_INSTR('hello', 'he(l+)o', 1, 1);
+SELECT REGEXP_INSTR('hello', 'he(l+)o', 1, 1, 0);
+SELECT REGEXP_INSTR('hello', 'he(l+)o', 1, 1, 0, 'i');
+SELECT REGEXP_INSTR('hello', 'he(l+)o', 1, 1, 0, 'c', 1);
+
+SELECT REGEXP_REPLACE('hello', 'he(l+)o');
+SELECT REGEXP_REPLACE('hello', 'he(l+)o', 'hi\1o');
+SELECT REGEXP_REPLACE('hello', 'he(l+)o', 'hi\1o', 1);
+SELECT REGEXP_REPLACE('hello', 'he(l+)o', 'hi\1o', 1, 1);
+SELECT REGEXP_REPLACE('hello', 'he(l+)o', 'hi\1o', 1, 1, 'm');
+
+SELECT REGEXP_SUBSTR('hello', 'he(l+)o');
+SELECT REGEXP_SUBSTR('hello', 'he(l+)o', 1);
+SELECT REGEXP_SUBSTR('hello', 'he(l+)o', 1, 1);
+SELECT REGEXP_SUBSTR('hello', 'he(l+)o', 1, 1, 's');
+SELECT REGEXP_SUBSTR('hello', 'he(l+)o', 1, 1, 'c', 1);
